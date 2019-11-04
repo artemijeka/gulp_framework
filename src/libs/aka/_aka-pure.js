7ea70d04df73e337f28f66614682e91e1ad8f6e4
artemijeka <artem.kuznecov.samara@gmail.com>
2019-11-04 14:23:01 +0400

    функции новые

--- src/libs/aka/_aka-pure.js
+++ src/libs/aka/_aka-pure.js
@@ -1,13 +1,23 @@
-function AKA() {
-  return {
-    maxWidth(maxPx) {
-      if ( (window.innerWidth
-          || document.documentElement.clientWidth
-          || document.body.clientWidth) <= maxPx) {
-      return true;
-      } else {
-      return false;
-      }
-    }
-  };
+function maxWidth(maxPx) {
+  if ((window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth) <= maxPx) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function minWidth(minPx) {
+  if ((window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth) >= minPx) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isMobile() {
+  return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
 }
