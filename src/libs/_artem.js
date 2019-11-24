58178ed5c038f62efe35d5082fb77f2d7c5f25ec
artemijeka <artem.kuznecov.samara@gmail.com>
2019-11-24 11:07:46 +0400

    upd

--- src/libs/_artem.js
+++ src/libs/_artem.js
@@ -1,6 +1,32 @@
-/**
- * dependencies: jQuery 
- */
+function maxWidth(maxPx) {
+  if ((window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth) <= maxPx) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function minWidth(minPx) {
+  if ((window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth) >= minPx) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isMobile() {
+  return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+}
+
+
+
+/****************************************************************************************************
+*********************************** Dependencies: jQuery ********************************************
+*****************************************************************************************************/
 $(function () {
 
   /**
