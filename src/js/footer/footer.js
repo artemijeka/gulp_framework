7cc0ec6b9a0c2dbd00fb91cb437915096ea385c0
artemijeka <artem.kuznecov.samara@gmail.com>
2019-12-20 15:15:51 +0400

    upd

--- src/js/footer/footer.js
+++
@@ -1,8 +0,0 @@
-$.getScript("../../libs/aka/aka-jquery.js", function(){
-  
-  typeOnlyNumber('.number');
-  typeOnlyText('.text');
-  // console.log(typeOnlyNumber('.number'));
-  // console.log(typeOnlyText('.text'));
-  
-});
--- src/js/header/_artem.js
+++ src/js/header/_artem.js
@@ -18,45 +18,11 @@ function minWidth(minPx) {
   }
 }
 
-function isMobile() {
-  return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
-}
-
-
 
 /****************************************************************************************************
 *********************************** Dependencies: jQuery ********************************************
 *****************************************************************************************************/
 $(function () {
-
-  /**
-   * inputSelectors = '#age' - one 
-   * inputSelectors = '#age, #phone' - or more
-   */
-  function typeOnlyNumber(inputsSelectors) {
-    $(document).on('keypress', inputsSelectors, function (event) {
-      let regex = new RegExp("^[1-9]+$");
-      let key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
-      if (!regex.test(key)) {
-        event.preventDefault();
-        return false;
-      }
-    });
-  }
-  /* 
-  * inputSelectors = '#name' - one 
-  * inputSelectors = '#name, #city' - or mores
-  */
-  function typeOnlyText(inputsSelectors) {
-    $(document).on('keypress', inputsSelectors, function (event) {
-      let regex = new RegExp("^[а-яА-ЯёЁ]+$");
-      let key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
-      if (!regex.test(key)) {
-        event.preventDefault();
-        return false;
-      }
-    });
-  }
 
   /*
   * Раздельные галереи на одной странице.  
