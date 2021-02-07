3dce39f6a0e468bcaf6af492f81171db4f4dc2f5
artem <work.kuznecov.samara@yandex.ru>
2021-02-07 12:57:49 +0400

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -42,14 +42,12 @@ const SRC = {
         './src/**/*.+(eot|svg|ttf|woff|woff2|mp4)', 
         './src/**/.htaccess',
         '!./src/**/*.html', 
-        '!./src/**/*.pug', 
-        '!./src/**/blocks', 
-        // './src/**/*.php', 
+        '!./src/**/*.pug',  
+        './src/**/*.php', 
         // './src/**/*.settings'
     ],
     PUG: [
         './src/**/*.pug', 
-        '!./src/blocks/*.pug'
     ],
     HTML: [
         './src/**/*.html'
@@ -354,7 +352,7 @@ gulp.task('run_server', function (done) {
         server: DEV.ROOT
     });
     gulp.watch(SRC.HTML, gulp.series(CONFIG.HTML.TYPE));//'html' || 'minhtml'
-    gulp.watch(SRC.PUG, gulp.series('pug'));
+    // gulp.watch(SRC.PUG, gulp.series('pug'));
     gulp.watch(SRC.SCSS.HEADER, gulp.series('scss_header'));
     gulp.watch(SRC.SCSS.FOOTER, gulp.series('scss_footer'));
     gulp.watch(SRC.SCSS.VENDOR.HEADER, gulp.series('scss_vendor_header'));
@@ -369,7 +367,7 @@ gulp.task('run_server', function (done) {
 
 
 
-gulp.task('default', gulp.series('clean_dev', 'pug', CONFIG.HTML.TYPE, 'move_files', 'scss_vendor_header', 'scss_vendor_footer', 'scss_header', 'scss_footer', 'js_vendor_header', 'js_vendor_footer', 'js_header', 'js_footer', 'imagemin', 'ewebp', 'run_server'));
+gulp.task('default', gulp.series('clean_dev', /* 'pug', */ CONFIG.HTML.TYPE, 'move_files', 'scss_vendor_header', 'scss_vendor_footer', 'scss_header', 'scss_footer', 'js_vendor_header', 'js_vendor_footer', 'js_header', 'js_footer', 'imagemin', 'ewebp', 'run_server'));
 
 
 
